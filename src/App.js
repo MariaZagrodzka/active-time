32647ad5b0ce6afac01312245a1bc179c4e995eb
Maria Zagrodzka <maria.zagrodzka1@gmail.com>
2018-06-16 17:25:00 +0200

    init commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,56 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// @flow
 
-class App extends Component {
+import React, { Component } from 'react'
+import logo from './logo.svg'
+
+class App extends React.Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Active Time</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <style jsx>{`
+          .App {
+            text-align: center;
+          }
+
+          .App-logo {
+            animation: App-logo-spin infinite 20s linear;
+            height: 80px;
+          }
+
+          .App-header {
+            background-color: #222;
+            height: 150px;
+            padding: 20px;
+            color: white;
+          }
+
+          .App-title {
+            font-size: 1.5em;
+          }
+
+          .App-intro {
+            font-size: large;
+          }
+
+          @keyframes App-logo-spin {
+            from {
+              transform: rotate(0deg);
+            }
+            to {
+              transform: rotate(360deg);
+            }
+          }
+        `}</style>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
