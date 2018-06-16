3cba123aa37759f486113abf08594fa99cbc891d
Maria Zagrodzka <maria.zagrodzka1@gmail.com>
2018-06-16 20:37:25 +0200

    create pages

---
+++ src/pages/Galery.js
@@ -0,0 +1,17 @@
+// @flow
+
+import React from 'react'
+
+const Galery = () => (
+  <div className="galery">
+    <style jsx>{`
+      .galery {
+        background: yellow;
+        width: 500px;
+        height: 500px;
+      }
+    `}</style>
+  </div>
+)
+
+export default Galery
---
+++ src/pages/PriceList.js
@@ -0,0 +1,17 @@
+// @flow
+
+import React from 'react'
+
+const PriceList = () => (
+  <div className="price-list">
+    <style jsx>{`
+      .price-list {
+        background: green;
+        width: 500px;
+        height: 500px;
+      }
+    `}</style>
+  </div>
+)
+
+export default PriceList
