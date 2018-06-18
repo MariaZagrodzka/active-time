8df15f94e8f6025cacec2921a6c4262448262067
JakubMarkiewicz <jakmar73@gmail.com>
2018-06-18 21:50:49 +0200

    Footer position fix

--- src/App.js
+++ src/App.js
@@ -1,47 +1,36 @@
 // @flow
 
-import React, { Component } from 'react'
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
-import Layout from './pages/Layout'
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Layout from "./pages/Layout";
 
 type stateType = {
   logged: boolean,
   token: string
-}
+};
 class App extends React.Component<void, stateType> {
   state = {
     logged: false,
-    token: ''
-  }
-  setToken = (token: string) => this.setState({ token })
+    token: ""
+  };
+  setToken = (token: string) => this.setState({ token });
   render() {
-    const { token } = this.state
+    const { token } = this.state;
     return (
       <div className="App">
         <Layout token={token} setToken={this.setToken} />
-        <footer className="footer">
-          Jeśli masz pytania, dzwoń: 123456789 :)
-        </footer>
+
         <style jsx>{`
           .App {
-            height: 100%;
+            min-height: 100vh;
             width: 100%;
             min-width: 400px;
-          }
-          .footer {
-            background: black;
-            width: 100%;
-            height: 5em;
-            color: rgba(200, 200, 200, 0.8);
-            font-size: 0.7em;
-            text-align: center;
-            padding-top: 1em;
-            bottom: 0;
+            position: relative;
           }
         `}</style>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,82 +1,94 @@
-import React from 'react'
-import Navbar from './Navbar'
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
-import Home from './Home'
-import About from './About'
-import Galery from './Galery'
-import Offer from './Offer'
-import Contact from './Contact'
-import Admin from './Admin'
-import Register from './Register'
+import React from "react";
+import Navbar from "./Navbar";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Galery from "./Galery";
+import Offer from "./Offer";
+import Contact from "./Contact";
+import Admin from "./Admin";
+import Register from "../components/Register";
 
 class Layout extends React.Component {
   getFromStorage = key => {
     if (!key) {
-      return null
+      return null;
     }
     try {
-      const valueStr = localStorage.getItem(key)
+      const valueStr = localStorage.getItem(key);
       if (valueStr) {
-        return JSON.parse(valueStr)
+        return JSON.parse(valueStr);
       }
-      return null
+      return null;
     } catch (err) {
-      return null
+      return null;
     }
-  }
+  };
   logout = () => {
-    const obj = this.getFromStorage('the_main_app')
+    const obj = this.getFromStorage("the_main_app");
     if (obj && obj.token) {
-      const { token } = obj
-      fetch('http://localhost:3001/api/account/logout?token=' + token, {
-        method: 'POST',
+      const { token } = obj;
+      fetch("http://localhost:3001/api/account/logout?token=" + token, {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json'
+          "Content-Type": "application/json"
         }
       })
         .then(res => res.json())
         .then(json => {
           if (json.success) {
-            this.props.setToken('')
-            localStorage.setItem('the_main_app', '')
+            this.props.setToken("");
+            localStorage.setItem("the_main_app", "");
           }
-        })
+        });
     }
-  }
+  };
   render() {
-    const { token, setToken } = this.props
+    const { token, setToken } = this.props;
     return (
       <div className="layout">
         <Router>
           <React.Fragment>
             <Navbar token={token} logout={this.logout} />
-            <div className="nav" />
-            <Route exact path="/" component={About} />
-            <Route path="/Galeria" component={Galery} />
-            <Route path="/Oferta" component={Offer} />
-            <Route path="/Kontakt" component={Contact} />
-            <Route
-              path="/Rejestracja"
-              render={props => (
-                <Register token={token} setToken={setToken} {...props} />
-              )}
-            />
+            <main>
+              <Route exact path="/" component={About} />
+              <Route path="/Galeria" component={Galery} />
+              <Route path="/Oferta" component={Offer} />
+              <Route path="/Kontakt" component={Contact} />
+              <Route
+                path="/Rejestracja"
+                render={props => (
+                  <Register token={token} setToken={setToken} {...props} />
+                )}
+              />
+            </main>
+            <footer className="footer">
+              Jeśli masz pytania, dzwoń: 123456789 :)
+            </footer>
           </React.Fragment>
         </Router>
         <style jsx>{`
           .layout {
             height: 100%;
           }
-
-          .nav {
+          main {
+            padding-top: 4.5em;
+          }
+          .footer {
             background: black;
             width: 100%;
-            height: 4.5em;
+            height: 5em;
+            color: rgba(200, 200, 200, 0.8);
+            font-size: 0.7em;
+            text-align: center;
+            padding-top: 1em;
+            bottom: 0;
+            position: absolute;
           }
         `}</style>
       </div>
-    )
+    );
   }
 }
 
-export default Layout
+export default Layout;
